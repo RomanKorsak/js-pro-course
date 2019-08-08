f07b03c12ea6d39c0eaa43e9ab9b26ba786b5b55
Roman <korsak-mc12@list.ru>
2019-08-08 13:32:25 +0300

    code refactoring

--- async-development/index.js
+++ async-development/index.js
@@ -2,41 +2,21 @@ let buttonFind = document.getElementById('buttonId');
 let ul = document.getElementById('ul');
 let clearAll = document.getElementById('clearAllButton');
 let myWeatherButton = document.getElementById('myWeatherButton');
+let input = document.getElementById('inputId');
 
 // add new city
 buttonFind.addEventListener('click', function(){   
     if(ul.childElementCount < 8){
-        let input = document.getElementById('inputId');
-        fetch('http://api.apixu.com/v1/current.json?key=61b56aaf9e934612b7d181411193007&q=' + input.value)
-        .then(response => response.json())
-        .then((info) => {
-        let li = document.createElement('li'); 
-        li.innerHTML = info.location.name + ', ' + info.location.country + ', ' + info.current.temp_c + '°C, ' + "Wind: " + info.current.wind_kph + " km/h ";
-        li.appendChild(deleteButton());
-        ul.appendChild(li); 
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-   input.value = '';
+        createCity();
     }else{
         alert('Почисти список!');
-}
+    }
 });
 
 // my city by location
 myWeatherButton.addEventListener('click', function(){ 
     if(ul.childElementCount < 8){      
-        navigator.geolocation.getCurrentPosition(({coords}) => myWeather(`${coords.latitude},${coords.longitude}`));
-        let myWeather = value => fetch(`http://api.apixu.com/v1/current.json?key=61b56aaf9e934612b7d181411193007&q=${value}`)
-        .then(response => response.json())
-        .then((info) => {
-        let li = document.createElement('li'); 
-        li.innerHTML = info.location.name + ', ' + info.location.country + ', ' + info.current.temp_c + '°C, ' + "Wind: " + info.current.wind_kph + " km/h";
-        li.appendChild(deleteButton());
-        ul.appendChild(li);
-        })
-        .catch(error => console.log(error));
+        addMtCityBYLlocation();
     }else{
         alert('Почисти список!')   
     }
@@ -51,7 +31,6 @@ function deleteButton(){
     liDeleteButton.addEventListener('click', function(event){
         ul.removeChild(event.target.parentNode);
     }) 
-
     return liDeleteButton;
 }
 
@@ -59,3 +38,35 @@ function deleteButton(){
 clearAll.addEventListener('click', function(){
     ul.innerHTML = '';
 })
+
+// add a new city function
+function createCity(){
+    fetch('http://api.apixu.com/v1/current.json?key=61b56aaf9e934612b7d181411193007&q=' + input.value)
+        .then(response => response.json())
+        .then((info) => {
+            createLi(info);
+        })
+    .catch((error) => {
+        console.log(error);
+    })
+    input.value = '';
+};
+
+// add a new city by your location
+function addMtCityBYLlocation(){
+    navigator.geolocation.getCurrentPosition(({coords}) => myWeather(`${coords.latitude},${coords.longitude}`));
+    let myWeather = value => fetch(`http://api.apixu.com/v1/current.json?key=61b56aaf9e934612b7d181411193007&q=${value}`)
+    .then(response => response.json())
+    .then((info) => {
+        createLi(info);
+    })
+    .catch(error => console.log(error));
+}
+
+//create new li with city
+function createLi(info){
+    let li = document.createElement('li'); 
+    li.innerHTML = info.location.name + ', ' + info.location.country + ', ' + info.current.temp_c + '°C, ' + "Wind: " + info.current.wind_kph + " km/h";
+    li.appendChild(deleteButton());
+    ul.appendChild(li); 
+}
