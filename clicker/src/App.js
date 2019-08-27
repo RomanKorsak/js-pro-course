e86cff4912af0e92fd32c695a864c7e6f517dba9
Roman <korsak-mc12@list.ru>
2019-08-27 16:53:22 +0300

    Clicker

---
+++ clicker/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+
+export default class Clicker extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {count: props.count};
+
+    this.incrementCount = this.incrementCount.bind(this);
+    this.decrementCount = this.decrementCount.bind(this);
+    this.resetCount = this.resetCount.bind(this);
+    
+  }
+
+  incrementCount() {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  }
+
+  decrementCount() {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  }
+
+  resetCount() {
+    this.setState(() => ({ count: Clicker.defaultProps.count }));
+  }
+
+  render(){
+    return (
+      <div className="clicker">
+        <div id = "output"> {this.state.count} </div>
+        <button id = "plusButton" onClick={this.incrementCount}>+</button>
+        <button id = "refreshButton" onClick={this.resetCount}>refresh</button>
+        <button id = "minusButton" onClick={this.decrementCount}>-</button>
+      </div>
+    );
+  }
+}
+
+Clicker.defaultProps = {
+  count: 0
+};
---
+++ clicker/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Clicker from './App';
+
+
+ReactDOM.render(<Clicker />, document.getElementById('root'));
+
+
